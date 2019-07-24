0aa4bebb0d8bbbc0aa6a774849d25dd92ad6b8fe
hiba21-meet <hiba21@meet.mit.edu>
2019-07-24 07:27:24 -0400

    Stopping here

---
+++ lab_4.py
@@ -0,0 +1,24 @@
+import turtle
+turtle.shape('square')
+turtle.shape('turtle')
+finn = turtle.clone()
+finn.shape('square')
+finn.goto(100,100)
+finn.goto(200,100)
+finn.goto(200,0)
+finn.goto(100,0)
+finn.goto(100,100)
+finn.penup()
+charlie = finn.clone()
+charlie.shape('triangle')
+charlie.pendown()
+charlie.goto(150,150)
+charlie.goto(200,100)
+finn.goto(100.100)
+finn.goto(300,300)
+finn.stamp()
+finn.goto(100,100)
+
+
+turtle.mainloop()
+
